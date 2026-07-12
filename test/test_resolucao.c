e9b2a9bdadd02ca0e5478c8b75f858990b3e3adb
BreaKnoh <brematosmene@gmail.com>
2026-07-12 13:41:50 -0300

    feat: adiciona teste para dama ambigua

--- test/test_resolucao.c
+++ test/test_resolucao.c
@@ -1,5 +1,6 @@
 #include "teste.h"
 /*
+  0 1 2 3 4 5 6 7
   A B C D E F G H
 1 . . C . . . . .
 2 B . B . . . . .
@@ -15,6 +16,33 @@ static char MOCK_AMBUIGUIDADE[9][9] = {
     "..C.....", "B.B.....", ".*T.....", "BT......",
     "C...D.D.", "........", "....D.*.", "........",
 }; // posicoes concorridas: b3 e g7
+
+TESTE(dama_ambigua) {
+  Tabuleiro tab = new_tabuleiro(MOCK_AMBUIGUIDADE);
+
+  struct {
+    char *comando;
+    struct {
+      i8 x;
+      i8 y;
+    } origem;
+    Resolucao resultado_esperado;
+  } testes[] = {
+      {"Dg7", {-1, -1}, AMBIGUO},    {"Deg7", {4, -1}, AMBIGUO},
+      {"D5g7", {-1, 4}, AMBIGUO},    {"D7g7", {-1, 6}, ENCONTRADA},
+      {"Dgg7", {6, -1}, ENCONTRADA}, {"De5g7", {4, 4}, ENCONTRADA},
+  };
+
+  for (int i = 0; i < 6; i++) {
+    Jogada jog = parse_comando(testes[i].comando, BRANCA);
+
+    ASSERT_EQ(jog.origem.x, testes[i].origem.x, d)
+
+    Resolucao resolucao = resolver_jogada(tab.posicoes, &jog);
+
+    ASSERT_EQ(testes[i].resultado_esperado, resolucao, c);
+  }
+}
 
 TESTE(cavalo_ambiguo) {
   Tabuleiro tab = new_tabuleiro(MOCK_AMBUIGUIDADE);
@@ -71,7 +99,7 @@ TESTE(bispo_ambiguo) {
   }
 }
 
-TESTE(torre_ambiguo) {
+TESTE(torre_ambigua) {
   Tabuleiro tab = new_tabuleiro(MOCK_AMBUIGUIDADE);
 
   struct {
